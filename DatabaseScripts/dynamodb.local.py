ede1f93b6feb1c9a80a192f22359d7c4f9e925f7
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2020-04-08 17:24:50 +0100

    Delete database

--- DatabaseScripts/dynamodb.local.py
+++ DatabaseScripts/dynamodb.local.py
@@ -1,4 +1,7 @@
 import boto3
 ddb = boto3.client('dynamodb', endpoint_url='http://localhost:8043/')
 response = ddb.list_tables()
+dynamodb = boto3.resource('dynamodb', region_name='eu-west-1', endpoint_url="http://localhost:8043")
+table = dynamodb.Table('requests_data')
+table.delete()
 print(response)
