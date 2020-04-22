e82ae99fee56ccd1f42443d8a37e18345f531b61
EC2 Default User <ec2-user@ip-172-31-87-163.ec2.internal>
2020-04-22 12:15:49 +0000

    o

--- DatabaseScripts/dynamodb.local.py
+++ DatabaseScripts/dynamodb.local.py
@@ -1,7 +1,9 @@
 import boto3
-ddb = boto3.client('dynamodb', endpoint_url='http://localhost:8043/')
+#ddb = boto3.client('dynamodb', endpoint_url='http://localhost:8043/')
+ddb = boto3.client('dynamodb', region_name="us-east-1")
 response = ddb.list_tables()
-dynamodb = boto3.resource('dynamodb', region_name='eu-west-1', endpoint_url="http://localhost:8043")
+#dynamodb = boto3.resource('dynamodb', region_name='eu-west-1', endpoint_url="http://localhost:8043")
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 table = dynamodb.Table('requests_data')
 table.delete()
 print(response)
